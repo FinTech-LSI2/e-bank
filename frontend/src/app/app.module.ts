ebc26c1b736750a9adbb47f962e0ab739f8ef53d
Ayman <aymangharib545@gmail.com>
2024-12-28 18:31:52 +0000

    added the dockefile for the front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component'; // Correct import
 import { RouterModule } from '@angular/router';
-import { CreditRequestComponent } from './components/credit-request/credit-request.component'; // Correct import
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // For [(ngModel)]
 
+// Component imports
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreditRequestComponent } from './components/credit-request/credit-request.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component'; // Add this if missing
 
 @NgModule({
   declarations: [
-      // Declare components here
-    // Declare your HomeComponent here
+    // Declare all components here
+    AppComponent,
+    HomeComponent,
+    CreditRequestComponent,
+    SearchBarComponent, // Add this for the search bar
   ],
-  imports: [AppComponent,BrowserModule, HomeComponent,FontAwesomeModule,FormsModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA], // You don't need to import CommonModule here for AppModule
-  bootstrap: [] // Bootstrap your root component
+  imports: [
+    BrowserModule, 
+    FontAwesomeModule, 
+    FormsModule, // To enable [(ngModel)]
+    RouterModule.forRoot([]), // Define your routes here if using routing
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Optional, remove if unnecessary
+  bootstrap: [AppComponent], // Specify the root component to bootstrap
 })
 export class AppModule {}
