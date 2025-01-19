b665b4b7740fad248d5b72081015315cd0497406
EREH <150424356+sihamErmk@users.noreply.github.com>
2025-01-19 03:13:47 +0100

    Delete Authentication_service directory

--- Authentication_service/src/main/java/com/banque/authentication_service/controller/CustomAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.banque.authentication_service.controller;
-
-public class CustomAuthenticationException extends RuntimeException {
-    public CustomAuthenticationException(String message) {
-        super(message);
-    }
-}
